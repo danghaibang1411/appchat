f70a71eb24320dc07aecf33b8d1a8928a9d0ec80
bangdang <bang.dang.hai1411@gmail.com>
2024-02-06 22:14:22 +0700

    create class

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SplashScreen } from './src/screens'
+import { NavigationContainer } from '@react-navigation/native'
+import AuthNavigator from './src/navigators/AuthNavigator'
+
+const App = () => {
+  const [isShowSplash, setIsShowSplash] = useState(true)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false)
+    }, 1500);
+    return () => clearTimeout(timeout)
+  }, [])
+  return isShowSplash ? (<SplashScreen />) : (<NavigationContainer>
+    <AuthNavigator />
+  </NavigationContainer>)
+}
+
+export default App
---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+import LoginScreen from "./auth/LoginScreen";
+import SplashScreen from "./SplashScreen";
+import HomeScreen from "./home/HomeScreen";
+
+export {HomeScreen, LoginScreen, SplashScreen}
