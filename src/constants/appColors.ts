f36175df7ebbb2abfc1bfebcf2d1d2f38518b2cb
bangdang <bang.dang.hai1411@gmail.com>
2024-02-07 09:52:37 +0700

    update splash screen

--- src/constants/appColors.ts
+++ src/constants/appColors.ts
@@ -1,3 +1,4 @@
 export const appColors = {
     white: '#FFF', 
+    gray: '#2F6',
 }
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, ActivityIndicator } from 'react-native'
 import React from 'react'
+import { Image } from 'react-native-reanimated/lib/typescript/Animated'
+import { appInfo } from "../constants/appInfo";
+import { SpaceComponent } from '../components';
+import { appColors } from '../constants/appColors';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      source={require('../assets/image/background.png')}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      imageStyle={{ flex: 1 }}>
+      <Image
+        source={require('../assets/image/splash-img.png')}
+        style={{
+          width: appInfo.sizes.WIDTH * 0.7,
+          resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={20}/>
+      <ActivityIndicator color={appColors.gray} size={22}/>
+    </ImageBackground>
+  );
+};
 
 export default SplashScreen
