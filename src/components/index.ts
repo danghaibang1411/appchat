12c5f1ef6e2e6c62cdfebf0802acb7aff9d151fa
bangdang <bang.dang.hai1411@gmail.com>
2024-02-28 21:25:57 +0700

    update Login Screen

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,17 @@
-import SpaceComponent from "./SpaceComponent";
+import ButtonComponent from './ButtonComponent';
+import ContainerComponent from './ContainerComponent';
+import InputComponent from './InputComponent';
+import RowComponent from './RowComponent';
+import SectionComponent from './SectionComponent';
+import SpaceComponent from './SpaceComponent';
+import TextComponent from './TextComponent';
 
-export {SpaceComponent};
+export {
+  SpaceComponent,
+  TextComponent,
+  ButtonComponent,
+  InputComponent,
+  SectionComponent,
+  ContainerComponent,
+  RowComponent,
+};
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,137 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {Lock, Sms} from 'iconsax-react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, Image, Switch} from 'react-native';
+import authenticationAPI from '../../apis/authApi';
+import {appColors} from '../../constants/appColors';
+import {Validate} from '../../utils/validate';
+import {useDispatch} from 'react-redux';
+import {addAuth} from '../../redux/reducers/authReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ContainerComponent from '../../components/ContainerComponent';
+import SectionComponent from '../../components/SectionComponent';
+import TextComponent from '../../components/TextComponent';
+import InputComponent from '../../components/InputComponent';
+import SpaceComponent from '../../components/SpaceComponent';
+import RowComponent from '../../components/RowComponent';
+import ButtonComponent from '../../components/ButtonComponent';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}: any) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRemember, setIsRemember] = useState(true);
+  const [isDisable, setIsDisable] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const emailValidation = Validate.email(email);
+
+    if (!email || !password || !emailValidation) {
+      setIsDisable(true);
+    } else {
+      setIsDisable(false);
+    }
+  }, [email, password]);
+
+  const handleLogin = async () => {
+    const emailValidation = Validate.email(email);
+    if (emailValidation) {
+      try {
+        const res = await authenticationAPI.HandleAuthentication(
+          '/login',
+          {email, password},
+          'post',
+        );
+
+        dispatch(addAuth(res.data));
+
+        await AsyncStorage.setItem(
+          'auth',
+          isRemember ? JSON.stringify(res.data) : email,
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      Alert.alert('Email is not correct!!!!');
+    }
+  };
+
   return (
-    <View>
-      <Text >LoginScreen</Text>
-    </View>
-  )
-}
+    <ContainerComponent isImageBackground isScroll>
+      <SectionComponent
+        styles={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 75,
+        }}>
+        <Image
+          source={require('../../assets/images/text-logo.png')}
+          style={{
+            width: 162,
+            height: 114,
+            marginBottom: 30,
+          }}
+        />
+      </SectionComponent>
+      <SectionComponent>
+        <TextComponent size={24} title text="Sign in" />
+        <SpaceComponent height={21} />
+        <InputComponent
+          value={email}
+          placeholder="Email"
+          onChange={val => setEmail(val)}
+          allowClear
+          affix={<Sms size={22} color={appColors.gray} />}
+        />
+        <InputComponent
+          value={password}
+          placeholder="Password"
+          onChange={val => setPassword(val)}
+          isPassword
+          allowClear
+          affix={<Lock size={22} color={appColors.gray} />}
+        />
+        <RowComponent justify="space-between">
+          <RowComponent onPress={() => setIsRemember(!isRemember)}>
+            <Switch
+              trackColor={{true: appColors.primary}}
+              thumbColor={appColors.white}
+              value={isRemember}
+              onChange={() => setIsRemember(!isRemember)}
+            />
+            <SpaceComponent width={4} />
+            <TextComponent text="Remember me" />
+          </RowComponent>
+          <ButtonComponent
+            text="Forgot Password?"
+            onPress={() => navigation.navigate('ForgotPassword')}
+            type="text"
+          />
+        </RowComponent>
+      </SectionComponent>
+      <SpaceComponent height={16} />
+      <SectionComponent>
+        <ButtonComponent
+          disable={isDisable}
+          onPress={handleLogin}
+          text="SIGN IN"
+          type="primary"
+        />
+      </SectionComponent>
+      <SocialLogin />
+      <SectionComponent>
+        <RowComponent justify="center">
+          <TextComponent text="Don’t have an account? " />
+          <ButtonComponent
+            type="link"
+            text="Sign up"
+            onPress={() => navigation.navigate('SignUpScreen')}
+          />
+        </RowComponent>
+      </SectionComponent>
+    </ContainerComponent>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
